7dd5f88d3e995f233249ab323a2940ba6e715364
Shane Gove <shanemgove@gmail.com>
2019-05-03 21:08:05 -0500

    Final

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+//This will hold all the data for the friends
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    }
+  ];
+  
+  // Here we export the var friends array. 
+  //This makes it accessible to other files using "require".
+  module.exports = friends;
